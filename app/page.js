a112b87ba4d489fd22d5d2eab543ab56c39e217e
abhilashrishi1 <abhilashrishi1@gmail.com>
2026-06-27 21:30:26 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,7 @@
-import { Suspense } from 'react';
+'use client';
+
 import JournifiApp from './JournifiApp';
 
 export default function Page() {
-  return (
-    <Suspense fallback={
-      <div style={{ minHeight:'100vh', background:'#0F1117', display:'flex', alignItems:'center', justifyContent:'center' }}>
-        <div style={{ color:'#C9993A', fontFamily:'serif', fontSize:24 }}>Journifi</div>
-      </div>
-    }>
-      <JournifiApp />
-    </Suspense>
-  );
+  return <JournifiApp />;
 }
