e98e3d6f41ce0ba2ada73f793bdde61938821567
abhilashrishi1 <abhilashrishi1@gmail.com>
2026-06-27 21:43:02 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,8 @@
-'use client';
-
 import dynamic from 'next/dynamic';
 
 const JournifiApp = dynamic(() => import('./JournifiApp'), { 
   ssr: false,
-  loading: () => (
-    <div style={{ minHeight:'100vh', background:'#0F1117', display:'flex', alignItems:'center', justifyContent:'center' }}>
-      <div style={{ color:'#C9993A', fontFamily:'serif', fontSize:24 }}>Journifi</div>
-    </div>
-  )
+  loading: () => null
 });
 
 export default function Page() {
